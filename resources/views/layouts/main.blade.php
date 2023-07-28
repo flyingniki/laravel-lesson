312b296e4d7db617c60612fc250aef195fb11c07
Petr Nikitin <flying_niki@mail.ru>
2023-07-28 17:08:58 +0300

    lesson 34 done

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,23 +15,30 @@
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link" aria-current="page" href="{{route('main.index')}}">Main</a>
+                                <a class="nav-link" aria-current="page" href="{{ route('main.index') }}">Main</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('post.index')}}">Posts</a>
+                                <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('about.index')}}">About</a>
+                                <a class="nav-link" href="{{ route('about.index') }}">About</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('contact.index')}}">Contacts</a>
+                                <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
                             </li>
+                            @can('view', auth()->user())
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('admin.post.index') }}">Admin</a>
+                                </li>
+                            @endcan
                         </ul>
                     </div>
                 </div>
